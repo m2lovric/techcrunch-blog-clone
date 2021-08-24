7f702ff98351ff80c32135220b0cc1cca4b98b36
m2lovric <m2lovric@gmail.com>
2021-08-24 22:05:19 +0200

    Blog clone init

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className='App'>
+      <aside className='sidebar'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/b/b9/TechCrunch_logo.svg' alt='logo' />
+        <p>Disrupt</p>
+        <p>Startups</p>
+        <p>Videos</p>
+        <p>Audio</p>
+        <p>Newsletters</p>
+      </aside>
+      <main></main>
+    </div>
+  );
+}
+
+export default App;

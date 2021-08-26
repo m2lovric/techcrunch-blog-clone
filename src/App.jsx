049435837f7bd3cf7e1ab2027b660d37faec8e44
m2lovric <m2lovric@gmail.com>
2021-08-26 20:53:00 +0200

    html update

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,17 @@ function App() {
     <div className='App'>
       <aside className='sidebar'>
         <img src='https://upload.wikimedia.org/wikipedia/commons/b/b9/TechCrunch_logo.svg' alt='logo' />
+        <p>Join Extra Crunch</p>
+        <p>Login</p>
+        <br />
+        <p>Search</p>
         <p>Disrupt</p>
         <p>Startups</p>
         <p>Videos</p>
         <p>Audio</p>
         <p>Newsletters</p>
+        <p>Extra Crunch</p>
+        <p>EC-1s</p>
       </aside>
       <main></main>
     </div>
